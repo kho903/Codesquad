f376dbea451c57af6a85945cc0604b7f201ba3aa
kho903 <gmldnr2222@naver.com>
2020-12-13 18:01:03 +0900

    쓸모없는 함수인자 제거

--- step-2.py
+++ step-2.py
@@ -5,28 +5,28 @@ cube_array = [
 ]
 
 
-def u_temp(cube_array, input_value):
+def u_temp(cube_array):
     temp1 = cube_array[0][0]
     temp2 = cube_array[0][1]
     temp3 = cube_array[0][2]
     return temp1, temp2, temp3
 
 
-def r_temp(cube_array, input_value):
+def r_temp(cube_array):
     temp1 = cube_array[0][2]
     temp2 = cube_array[1][2]
     temp3 = cube_array[2][2]
     return temp1, temp2, temp3
 
 
-def l_temp(cube_array, input_value):
+def l_temp(cube_array):
     temp1 = cube_array[0][0]
     temp2 = cube_array[1][0]
     temp3 = cube_array[2][0]
     return temp1, temp2, temp3
 
 
-def b_temp(cube_array, input_value):
+def b_temp(cube_array):
     temp1 = cube_array[2][0]
     temp2 = cube_array[2][1]
     temp3 = cube_array[2][2]
@@ -35,35 +35,35 @@ def b_temp(cube_array, input_value):
 
 def solution(cube_array, input_value):
     if input_value == 'U':
-        temp1, temp2, temp3 = u_temp(cube_array, input_value)
+        temp1, temp2, temp3 = u_temp(cube_array)
         cube_array[0][0], cube_array[0][1], cube_array[0][2] = temp2, temp3, temp1
 
     elif input_value == 'U\'':
-        temp1, temp2, temp3 = u_temp(cube_array, input_value)
+        temp1, temp2, temp3 = u_temp(cube_array)
         cube_array[0][0], cube_array[0][1], cube_array[0][2] = temp3, temp1, temp2
 
     elif input_value == 'R':
-        temp1, temp2, temp3 = r_temp(cube_array, input_value)
+        temp1, temp2, temp3 = r_temp(cube_array)
         cube_array[0][2], cube_array[1][2], cube_array[2][2] = temp2, temp3, temp1
 
     elif input_value == 'R\'':
-        temp1, temp2, temp3 = r_temp(cube_array, input_value)
+        temp1, temp2, temp3 = r_temp(cube_array)
         cube_array[0][2], cube_array[1][2], cube_array[2][2] = temp3, temp1, temp2
 
     elif input_value == 'L':
-        temp1, temp2, temp3 = l_temp(cube_array, input_value)
+        temp1, temp2, temp3 = l_temp(cube_array)
         cube_array[0][0], cube_array[1][0], cube_array[2][0] = temp3, temp1, temp2
 
     elif input_value == 'L\'':
-        temp1, temp2, temp3 = l_temp(cube_array, input_value)
+        temp1, temp2, temp3 = l_temp(cube_array)
         cube_array[0][0], cube_array[1][0], cube_array[2][0] = temp2, temp3, temp1
 
     elif input_value == 'B':
-        temp1, temp2, temp3 = b_temp(cube_array, input_value)
+        temp1, temp2, temp3 = b_temp(cube_array)
         cube_array[0][0], cube_array[1][0], cube_array[2][0] = temp3, temp1, temp2
 
     elif input_value == 'B\'':
-        temp1, temp2, temp3 = b_temp(cube_array, input_value)
+        temp1, temp2, temp3 = b_temp(cube_array)
         cube_array[0][0], cube_array[1][0], cube_array[2][0] = temp2, temp3, temp1
 
     return cube_array
