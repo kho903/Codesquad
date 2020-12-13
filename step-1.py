62467dd5118b862637845c086a0d5a7100a36e33
kho903 <gmldnr2222@naver.com>
2020-12-13 18:47:35 +0900

    수정

--- step-1.py
+++ step-1.py
@@ -1,22 +1,23 @@
+def reverse_direction(direction):
+    if direction.lower() == 'r':
+        direction = 'l'
+    elif direction.lower() == 'l':
+        direction = 'r'
+    return direction
+
+
 def solution(word, int_value, direction):
     int_value = int(int_value)
+
+    if int_value < 0:
+        direction = reverse_direction(direction)
+        int_value = abs(int_value)
+    int_value %= len(word)
     if direction.lower() == 'l':
-        if int_value >= 0:
-            int_value %= len(word)
-            result = word[int_value:] + word[:int_value]
-        elif int_value < 0:
-            while len(word) < int_value:
-                int_value += len(word)
-            result = word
+        return word[int_value:] + word[:int_value]
 
     elif direction.lower() == 'r':
-        if int_value >= 0:
-            result = word[int_value:] + word[:int_value]
-        elif int_value < 0:
-            while len(word) < int_value:
-                int_value += len(word)
-            result = word[int_value:] + word[:int_value]
-    return result
+        return word[-int_value:] + word[:int_value]
 
 
 input_value = input("> ")
